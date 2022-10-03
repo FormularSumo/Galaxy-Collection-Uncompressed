33d865fbb965f0c4a4092c96b69e14f3604c062f
FormularSumo <chewy.h@live.co.uk>
2022-10-04 00:24:33 +0100

    Create Website fuse (unfinished).ps1

---
+++ Original Sources and Assets/Icons/Website fuse (unfinished).ps1
@@ -0,0 +1,3 @@
+Compress-Archive -Path "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\*" -DestinationPath "C:\Users\chewy\Documents\SWGC\Website\game.zip" -Force
+
+Rename-Item -Path "C:\Users\chewy\Documents\SWGC\Website\game.zip" -NewName "C:\Users\chewy\Documents\SWGC\Website\game.love"
