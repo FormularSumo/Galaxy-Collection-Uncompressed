fe2df8fac7292e798721daf0222167abdca8bf82
FormularSumo <chewy.h@live.co.uk>
2022-10-05 16:35:27 +0100

    Update Website fuse (unfinished).ps1
    Otherwise renaming doesn't work

--- Original Sources and Assets/Icons/Website fuse (unfinished).ps1
+++ Original Sources and Assets/Icons/Website fuse (unfinished).ps1
@@ -1,3 +1,5 @@
+Remove-Item "C:\Users\chewy\Documents\SWGC\Website\*" -Recurse
+
 Compress-Archive -Path "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\*" -DestinationPath "C:\Users\chewy\Documents\SWGC\Website\game.zip" -Force
 
 Rename-Item -Path "C:\Users\chewy\Documents\SWGC\Website\game.zip" -NewName "C:\Users\chewy\Documents\SWGC\Website\game.love"
