92d46a950ca85fefed44336a8480a7b318e9163f
FormularSumo <chewy.h@live.co.uk>
2022-10-05 17:15:29 +0100

    Update Website fuse (unfinished).ps1
    It now compiles the game.love file after creating it, and then copies the new files into the existing website folder, and then hosts a local server to run the website

--- Original Sources and Assets/Icons/Website fuse (unfinished).ps1
+++ Original Sources and Assets/Icons/Website fuse (unfinished).ps1
@@ -3,3 +3,14 @@ Remove-Item "C:\Users\chewy\Documents\SWGC\Website\*" -Recurse
 Compress-Archive -Path "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\*" -DestinationPath "C:\Users\chewy\Documents\SWGC\Website\game.zip" -Force
 
 Rename-Item -Path "C:\Users\chewy\Documents\SWGC\Website\game.zip" -NewName "C:\Users\chewy\Documents\SWGC\Website\game.love"
+
+npx love.js.cmd -m 150000000 -t "Galaxy Collection" "C:\Users\chewy\Documents\SWGC\Website\game.love" "C:\Users\chewy\Documents\SWGC\Website unmodified" -c
+
+Remove-Item "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Web\game.data" -Recurse
+
+cmd /c copy "C:\Users\chewy\Documents\SWGC\Website unmodified\game.data" "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Web\game.data"
+cmd /c copy "C:\Users\chewy\Documents\SWGC\Website unmodified\game.js" "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Web\game.js"
+
+cd C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Web
+
+python -m http.server 8000
