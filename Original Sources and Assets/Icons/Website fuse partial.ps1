37df6e81a7390d30f2b9d5fb1200f8b797e94f3e
FormularSumo <chewy.h@live.co.uk>
2022-10-09 16:14:17 +0100

    Add js support

--- Original Sources and Assets/Icons/Website fuse partial.ps1
+++ Original Sources and Assets/Icons/Website fuse partial.ps1
@@ -1,5 +1,9 @@
 npx love.js.cmd -m 150000000 -t "Galaxy Collection" "C:\Users\chewy\Documents\SWGC\Website\game.love" "C:\Users\chewy\Documents\SWGC\Website unmodified" -c
 
+cd "C:\Users\chewy\Documents\SWGC\Website unmodified"
+
+node globalizeFS.js
+
 Remove-Item "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Web\game.data" -Recurse
 
 cmd /c copy "C:\Users\chewy\Documents\SWGC\Website unmodified\game.data" "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Web\game.data"
--- Original Sources and Assets/Icons/Website fuse.ps1
+++ Original Sources and Assets/Icons/Website fuse.ps1
@@ -1,10 +1,14 @@
-Remove-Item "C:\Users\chewy\Documents\SWGC\Website\*" -Recurse
+Remove-Item "C:\Users\chewy\Documents\SWGC\Website\game.love"
 
 Compress-Archive -Path "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\*" -DestinationPath "C:\Users\chewy\Documents\SWGC\Website\game.zip" -Force
 
 Rename-Item -Path "C:\Users\chewy\Documents\SWGC\Website\game.zip" -NewName "C:\Users\chewy\Documents\SWGC\Website\game.love"
 
 npx love.js.cmd -m 150000000 -t "Galaxy Collection" "C:\Users\chewy\Documents\SWGC\Website\game.love" "C:\Users\chewy\Documents\SWGC\Website unmodified" -c
+
+cd "C:\Users\chewy\Documents\SWGC\Website unmodified"
+
+node globalizeFS.js
 
 Remove-Item "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Web\game.data" -Recurse
 
