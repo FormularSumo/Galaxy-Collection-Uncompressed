deff75e354c1d97c06c30873fc109bf3c429602c
FormularSumo <chewy.h@live.co.uk>
2022-11-19 16:26:16 +0000

    Update Android Fuse.ps1
    To use https://github.com/FormularSumo/Star-Wars-Galaxy-Collection-Android, which is now based on Love2d 11.4

--- Original Sources and Assets/Icons/Android Fuse.ps1
+++ Original Sources and Assets/Icons/Android Fuse.ps1
@@ -1,5 +1,5 @@
-Remove-Item "C:\Users\chewy\Documents\SWGC\love-android\app\src\main\assets\*" -Recurse
+Remove-Item "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Android\app\src\embed\assets\*" -Recurse
 
-Compress-Archive -Path "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\*" -DestinationPath "C:\Users\chewy\Documents\SWGC\love-android\app\src\main\assets\game.zip" -Force
+Compress-Archive -Path "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\*" -DestinationPath "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Android\app\src\embed\assets\game.zip" -Force
 
-Rename-Item -Path "C:\Users\chewy\Documents\SWGC\love-android\app\src\main\assets\game.zip" -NewName "C:\Users\chewy\Documents\SWGC\love-android\app\src\main\assets\game.love"
+Rename-Item -Path "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Android\app\src\embed\assets\game.zip" -NewName "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Android\app\src\embed\assets\game.love"
