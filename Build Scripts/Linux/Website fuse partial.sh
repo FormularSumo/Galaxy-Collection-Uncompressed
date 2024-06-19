e246a9ffd549393f9e2407fcc416725ae3a4de35
FormularSumo <chewy.h@live.co.uk>
2024-06-19 01:15:30 +0100

    Add Linux Website fuse partial
    Not quite sure why it needs to be python3 to work here. Also the 8000 is default so optional.

---
+++ Build Scripts/Linux/Website fuse partial.sh
@@ -0,0 +1,11 @@
+npx love.js -m 150000000 -t "Galaxy Collection" "/home/james/Documents/SWGC/Website/game.love" "/home/james/Documents/SWGC/Website unmodified" -c
+
+cd "/home/james/Documents/SWGC/Website unmodified"
+
+rm -r "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web/game.data" "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web/game.js"
+
+cp "/home/james/Documents/SWGC/Website unmodified/game.data" "/home/james/Documents/SWGC/Website unmodified/game.js" "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web/"
+
+cd /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web
+
+python3 -m http.server
