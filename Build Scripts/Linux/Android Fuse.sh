68152ada6ac25830fb01ca1570c7b6743c085c2b
FormularSumo <chewy.h@live.co.uk>
2024-08-23 23:44:32 +0100

    Add Linux Android Fuse script

---
+++ Build Scripts/Linux/Android Fuse.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+cd /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection; zip -9 -r -FSr /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Android/app/src/embed/assets/game.love *
