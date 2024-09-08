d93aea751d3c00c7880f0a87b5273b50feaefa70
FormularSumo <chewy.h@live.co.uk>
2024-09-08 20:33:08 +0200

    Modify Linux fuse to call executable love, final output Galaxy Collection
    Previously both were Star Wars Galaxy Collection. The Star Wars probably wanted to go anyway, but I didn't want to change the executable to love, but it's the only way I can get it to run at the moment. I've also added the working AppStreamData to the Linux folder, which I've tried to attach to the AppImage but won't attach without the AppImage renaming and breaking it. And finally moved the Windows VersionInfo into the Window folder

--- Build Scripts/Linux/Linux Fuse.sh
+++ Build Scripts/Linux/Linux Fuse.sh
@@ -2,8 +2,8 @@
 
 cd /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection; zip -9 -r -FSr /home/james/Documents/SWGC/game.love *
 
-cat /home/james/Documents/SWGC/Linux-AppImage/squashfs-root/bin/love /home/james/Documents/SWGC/game.love > /home/james/Documents/SWGC/Linux-AppImage/squashfs-root-modified/bin/"Star Wars Galaxy Collection"
+cat /home/james/Documents/SWGC/Linux-AppImage/squashfs-root/bin/love /home/james/Documents/SWGC/game.love > /home/james/Documents/SWGC/Linux-AppImage/squashfs-root-modified/bin/love
 
-chmod +x /home/james/Documents/SWGC/Linux-AppImage/squashfs-root-modified/bin/"Star Wars Galaxy Collection"
+chmod +x /home/james/Documents/SWGC/Linux-AppImage/squashfs-root-modified/bin/love
 
-/home/james/Documents/SWGC/Linux-AppImage/appimagetool-x86_64.AppImage /home/james/Documents/SWGC/Linux-AppImage/squashfs-root-modified /home/james/Documents/SWGC/Linux-AppImage/"Star Wars Galaxy Collection".AppImage
+/home/james/Documents/SWGC/Linux-AppImage/appimagetool-x86_64.AppImage /home/james/Documents/SWGC/Linux-AppImage/squashfs-root-modified /home/james/Documents/SWGC/Linux-AppImage/"Galaxy Collection".AppImage
