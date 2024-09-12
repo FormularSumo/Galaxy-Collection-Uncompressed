e1b25b99190e28c230b0b86e725dde3033384dc8
FormularSumo <chewy.h@live.co.uk>
2024-09-12 22:38:17 +0200

    Web --> website fuse, delete old artifact after build
    Windows script will probably be updated at a late stage, I don't really build Web from there anymore

---
+++ Build Scripts/Linux/Web fuse.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+cd /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection; zip -9 -r -FSr /home/james/Documents/SWGC/gameWeb.love *
+
+love.js -m 150000000 -t "Galaxy Collection" "/home/james/Documents/SWGC/gameWeb.love" "/home/james/Documents/SWGC/Web output" -c
+
+cd "/home/james/Documents/SWGC/Web output"
+
+rm -r "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web/game.data" "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web/game.js"
+
+cp "/home/james/Documents/SWGC/Web output/game.data" "/home/james/Documents/SWGC/Web output/game.js" "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web/"
+
+rm -r "/home/james/Documents/SWGC/Web output"
+
+cd /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web
+
+konsole -e python3 -m http.server
