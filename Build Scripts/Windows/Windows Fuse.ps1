0f0d65cc89d09713f35471f34ad05e035114a92c
FormularSumo <36278229+FormularSumo@users.noreply.github.com>
2024-09-20 21:27:35 +0200

    Update Windows fuse with new locations
    For logo and desktop repo

--- Build Scripts/Windows/Windows Fuse.ps1
+++ Build Scripts/Windows/Windows Fuse.ps1
@@ -1,17 +1,17 @@
 Compress-Archive -Path "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\*" -DestinationPath "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\game.zip" -Force
 
-cmd /c copy /b "C:\Program Files\LOVE\love.exe" + "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\game.zip" "C:\Users\chewy\Documents\SWGC\Galaxy Collection\Galaxy Collection.exe"
+cmd /c copy /b "C:\Program Files\LOVE\love.exe" + "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\game.zip" "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Desktop\Windows\Galaxy Collection.exe"
 
 Start-Sleep -Milliseconds 500
 
-& "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "C:\Users\chewy\Documents\SWGC\Galaxy Collection\Galaxy Collection.exe" -save "C:\Users\chewy\Documents\SWGC\Galaxy Collection\Galaxy Collection.exe" -resource "C:\Users\chewy\Documents\SWGC\Uncompressed-files-Star-Wars-Galaxy-Collection\Original Sources and Assets\Icons\Game.ico" -action addoverwrite - mask ICONGROUP,1,1
+& "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Desktop\Windows\Galaxy Collection.exe" -save "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Desktop\Windows\Galaxy Collection.exe" -resource "C:\Users\chewy\Documents\SWGC\Uncompressed-files-Star-Wars-Galaxy-Collection\Logos\Game.ico" -action addoverwrite - mask ICONGROUP,1,1
 
 Start-Sleep -Milliseconds 500
 
-& "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "C:\Users\chewy\Documents\SWGC\Galaxy Collection\Galaxy Collection.exe" -save "C:\Users\chewy\Documents\SWGC\Galaxy Collection\Galaxy Collection.exe" -resource "C:\Users\chewy\Documents\SWGC\Uncompressed-files-Star-Wars-Galaxy-Collection\Build Scripts\VersionInfo.res" -action addoverwrite
+& "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Desktop\Windows\Galaxy Collection.exe" -save "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Desktop\Windows\Galaxy Collection.exe" -resource "C:\Users\chewy\Documents\SWGC\Uncompressed-files-Star-Wars-Galaxy-Collection\Build Scripts\VersionInfo.res" -action addoverwrite
 
 Start-Sleep -Milliseconds 1000
 
-Compress-Archive -Path "C:\Users\chewy\Documents\SWGC\Galaxy Collection\*" -DestinationPath "C:\Users\chewy\Documents\SWGC\Galaxy Collection.zip" -Force
+Compress-Archive -Path "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Desktop\Windows\*" -DestinationPath "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection-Desktop\Galaxy Collection.zip" -Force
 
 Remove-Item "C:\Users\chewy\Documents\SWGC\Star-Wars-Galaxy-Collection\game.zip"
