70967063e0e69e8c4f4fff5e5ff68a8de7a8c89c
FormularSumo <chewy.h@live.co.uk>
2024-09-20 20:09:22 +0200

    Remove rm game.js, add Web-Testing fuses

--- Build Scripts/Linux/Web fuse partial.sh
+++ Build Scripts/Linux/Web fuse partial.sh
@@ -4,7 +4,7 @@ love.js -m 150000000 -t "Galaxy Collection" "/home/james/Documents/SWGC/gameWeb.
 
 cd "/home/james/Documents/SWGC/Web output"
 
-rm -r "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web/game.data" "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web/game.js"
+rm -r "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web/game.data"
 
 cp "/home/james/Documents/SWGC/Web output/game.data" "/home/james/Documents/SWGC/Web output/game.js" "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web/"
 
