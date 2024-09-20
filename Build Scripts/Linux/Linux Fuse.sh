968ff3baf8d64a9b1d7dff9e58fc5e77469363bd
FormularSumo <chewy.h@live.co.uk>
2024-09-20 21:31:24 +0200

    Update Linux and Web build scripts with new locations

--- Build Scripts/Linux/Linux Fuse.sh
+++ Build Scripts/Linux/Linux Fuse.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-cd /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection; zip -9 -r -FSr /home/james/Documents/SWGC/game.love *
+cd /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection; zip -9 -r -FSr /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/game.love *
 
-cat /home/james/Documents/SWGC/Linux-AppImage/squashfs-root/bin/love /home/james/Documents/SWGC/game.love > /home/james/Documents/SWGC/Linux-AppImage/squashfs-root-modified/bin/love
+cat /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/squashfs-root/bin/love /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/game.love > /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/squashfs-root-modified/bin/love
 
-chmod +x /home/james/Documents/SWGC/Linux-AppImage/squashfs-root-modified/bin/love
+chmod +x /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/squashfs-root-modified/bin/love
 
-/home/james/Documents/SWGC/Linux-AppImage/appimagetool-x86_64.AppImage /home/james/Documents/SWGC/Linux-AppImage/squashfs-root-modified /home/james/Documents/SWGC/Linux-AppImage/"Galaxy Collection".AppImage
+/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/appimagetool-x86_64.AppImage /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/squashfs-root-modified /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/"Galaxy Collection".AppImage
---
+++ Build Scripts/Linux/Web-Testing fuse partial.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+love.js -m 150000000 -t "Galaxy Collection" "/home/james/Documents/SWGC/gameWeb.love" "/home/james/Documents/SWGC/Web output" -c
+
+cd "/home/james/Documents/SWGC/Web output"
+
+rm -r "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web-Testing/game.data"
+
+cp "/home/james/Documents/SWGC/Web output/game.data" "/home/james/Documents/SWGC/Web output/game.js" "/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web-Testing/"
+
+cd /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Web-Testing
+
+konsole -e python3 -m http.server
