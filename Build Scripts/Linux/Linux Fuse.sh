c5135a3d5c2455314ec7aa8004611960cdf50a88
FormularSumo <chewy.h@live.co.uk>
2024-09-20 21:33:48 +0200

    Update Linux build script to output AppImage to root folder like Windows ZIP

--- Build Scripts/Linux/Linux Fuse.sh
+++ Build Scripts/Linux/Linux Fuse.sh
@@ -6,4 +6,4 @@ cat /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImag
 
 chmod +x /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/squashfs-root-modified/bin/love
 
-/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/appimagetool-x86_64.AppImage /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/squashfs-root-modified /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/"Galaxy Collection".AppImage
+/home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/appimagetool-x86_64.AppImage /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/Linux-AppImage/squashfs-root-modified /home/james/Documents/SWGC/Star-Wars-Galaxy-Collection-Desktop/"Galaxy Collection".AppImage
